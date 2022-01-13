786a011ee2336dbd32cf013de78db8b55ca40936
Ridegras <franciscominutti.fm@gmail.com>
2022-01-13 15:37:45 -0300

    debugging

--- client/src/index.js
+++ client/src/index.js
@@ -7,7 +7,6 @@ import { Provider } from 'react-redux';
 import {store} from './store';
 import axios from 'axios';
 
-require('dotenv').config();
 axios.defaults.baseURL = process.env.REACT_APP_API || "http://localhost:3001";
 
 ReactDOM.render(
